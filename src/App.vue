b431fbef683273898bf3075876595f86419229d5
yyd <609825025@qq.com>
2018-11-09 10:13:27 +0800

    修改

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,46 @@
 <template>
-  <div class="App">	
-	<transition name="router-fade" mode="out-in">
-		<router-view></router-view>	
-	</transition>
-	<div id="goTop"></div>	
+  <div class="App">
+    <transition name="router-fade" mode="out-in">
+        <router-view></router-view>
+    </transition>
+
+    <loading
+        :showLoading="isLoading"
+        :showRefreshBt="showRefreshBt"
+        :status="status"
+    />
   </div>
 </template>
 
 <script>
-	import { Loading } from 'vux';
-	import { mapState } from 'vuex';
-	import { goTop } from 'js/yydjs.js';
+    import loading from 'components/common/loading';
+    import {mapState} from 'vuex';
 
-	export default {
-		computed:{
-			...mapState({
-		      isLoading: state => state.isLoading,
-		    })
-		},
-		mounted(){
-			//回到顶部
-			goTop(document.getElementById('goTop'));
-		},
-		components:{Loading} 
-	}
+    export default {
+        computed:{
+            ...mapState({
+                isLoading(state){
+                    return state.isLoading;
+                },
+                showRefreshBt(state){
+                    return state.showRefreshBt;
+                },
+                status(state){
+                    return state.status;
+                },
+            })
+        },
+
+        mounted(){
+
+        },
+
+        components:{
+            loading,
+        },
+    }
 </script>
 
 <style>
-	@import './css/yydCommon.css';   	
+    @import '~css/index.css';
 </style>
--- src/router.js
+++ src/router.js
@@ -1,120 +1,44 @@
-import App from './App.vue'
+import App from 'src/App';
 
 //定义组件
 //测试
-const test = r => require.ensure([], () => r(require('./pages/test/test.vue')), 'test');
+const test=()=>import('pages/test/test');
 
 //首页
-const home = r => require.ensure([], () => r(require('./pages/home/home.vue')), 'home');
+const home=()=>import('pages/home/home');
 
-//新手专区
-const noviceArea = r => require.ensure([], () => r(require('./pages/noviceArea/noviceArea.vue')), 'noviceArea');
-//新手专区首页
-const noviceAreaIndex = r => require.ensure([], () => r(require('./pages/noviceArea/children/noviceAreaIndex.vue')), 'noviceAreaIndex');
-//新手专区详情
-const noviceAreaDetail = r => require.ensure([], () => r(require('./pages/noviceArea/children/noviceAreaDetail.vue')), 'noviceAreaDetail');
-
-//资讯
-const infomation = r => require.ensure([], () => r(require('./pages/infomation/infomation.vue')), 'infomation');
-//资讯详情首页
-const infomationIndex = r => require.ensure([], () => r(require('./pages/infomation/children/infomationIndex.vue')), 'infomationIndex');
-//评论详情
-const commentDetail = r => require.ensure([], () => r(require('./pages/infomation/children/commentDetail.vue')), 'commentDetail');
-
-//公用内联框架
-const publicIframe = r => require.ensure([], () => r(require('./pages/publicIframe/publicIframe.vue')), 'publicIframe');
 
 //404页面
-const page404 = r => require.ensure([], () => r(require('./pages/page404/page404.vue')), 'page404');
+const page404=()=>import('pages/page404/page404');
+
 
 //配置路由规则
-export default [{
-    path: '/',
-    component: App, //顶层路由，对应index.html
-    children: [ //二级路由。对应App.vue
-    	{
+export default[{
+    path:'/',
+    component:App, //顶层路由，对应index.html
+    children:[ //二级路由。对应App.vue
+        {
             path:'',
-            redirect:'/test',      	
+            redirect:'/home',
         },
         //测试页面
         {
-			path:'/test', 
-			component:test,
-			meta:{
-				noLogin:true,
-			},
-		},	
-		//首页
-		{
-			path:'/home',
-			component:home,	
-			meta:{
-				noLogin:true,
-			},	
-		},
-		//新手专区
-		{
-			path:'/noviceArea',
-			component:noviceArea,
-			children:[
-				{
-					path:'', 
-					component:noviceAreaIndex,
-					meta:{
-						noLogin:true,
-					},
-				},
-				{
-					path:'noviceAreaDetail', 
-					component:noviceAreaDetail,
-					meta:{
-						noLogin:true,
-					},
-				},
-			],	
-			meta:{
-				noLogin:true,
-			},	
-		},
-		//资讯
-		{
-			path:'/infomation',
-			component:infomation,
-			children:[
-				{
-					path:'', 
-					component:infomationIndex,
-					meta:{
-						noLogin:true,
-					},
-				},
-				{
-					path:'commentDetail', 
-					component:commentDetail,
-					meta:{
-						noLogin:true,
-					},
-				},
-			],	 
-			meta:{
-				noLogin:true,
-			},	
-		},
-		//公用内联框架
-		{
-			path:'publicIframe',
-			component:publicIframe,
-			meta:{
-				noLogin:true,
-			},
-		},
-		//404页面
-		{
-			path:'*',
-			component:page404,
-			meta:{
-				noLogin:true,
-			},
-		},
+            path:'/test',
+            component:test,
+            meta:{
+                noLogin:true,
+                keepPos:true,
+            },
+        },
+        //首页
+        {
+            path:'/home',
+            component:home,
+        },
+        //404页面
+        {
+            path:'*',
+            component:page404,
+        },
     ]
 }]
