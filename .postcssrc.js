3022b0a89c591734e862676171c3cee533bdf503
yyd <609825025@qq.com>
2019-01-11 10:45:10 +0800

    修改

--- .postcssrc.js
+++ .postcssrc.js
@@ -6,7 +6,7 @@ module.exports = {
         "autoprefixer": {},
         "postcss-pxtorem": {
             "rootValue": 100,
-            "minPixelValue": 2,
+            "minPixelValue": 2, //如px小于这个值，就不会转换了
             "propList": ["*"], // 如需开启pxToRem模式，请在数组中加入"*"
             "selectorBlackList": [] //如需把css选择器加入黑名单，请在数组中加入对应的前缀，比如"mint-"
         }
